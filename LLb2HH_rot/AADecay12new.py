98d3ac7622a531525e3c987e66465eae266cddec
Jakub Wiechnik <kuba.wiechnik23@gmail.com>
2023-02-05 12:18:15 +0100

    AADecay12 class adjustment

--- LLb2HH_rot/AADecay12new.py
+++ LLb2HH_rot/AADecay12new.py
@@ -9,23 +9,6 @@ class AADecay12new:
         self.A = np.zeros((4, 4))
         self.A[0, 0] = 1
     
-    def get_kine_calc_trig(self, th, ph, ch):
-        """_This function takes kinematic parameters, saves those parameters
-        as class instance attributes and calculates trigonometric functions.
-        Those functions are also saved as class instance attributes._
-
-        Args:
-            th (_float_): _kinematic parameter_
-            ph (_float_): _kinematic parameter_
-            ch (_float_): _kinematic parameter_
-        """
-
-        self.th = th
-        self.ph = ph
-        self.ch = ch
-        self.RM = np.zeros((4,4))
-
-    
     def create_matrix(self):
         """_This functions creates a decay matrix. Martix elements
         are filled via class instance atributes._
@@ -38,19 +21,9 @@ class AADecay12new:
         aY = self.aY
         aZ = self.aZ
 
-        self.vec4 = np.array([1, aX, aY, aZ])
-
-        th = self.th
-        ph = self.ph
-        ch = self.ch
-        RM = RotMat(th, ph, ch)
-        R = RM.create()
-
-        self.A = np.matmul(R, self.vec4)
-
-        # self.A[1, 1] = R[1,:]*aX
-        # self.A[2, 2] = R[2,:]*aY
-        # self.A[3, 3] = R[3,:]*aZ
+        self.A[1, 0] = aX
+        self.A[2, 0] = aY
+        self.A[3, 0] = aZ
 
         return self.A
 
