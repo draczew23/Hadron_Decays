5fb9e52b95db3e33f6e684cb795a9bb74d789cf4
Jakub Wiechnik <kuba.wiechnik23@gmail.com>
2023-02-08 12:58:57 +0100

    Addition of generating parameters function

--- LLb2HH_rot/AADecay12new.py
+++ LLb2HH_rot/AADecay12new.py
@@ -9,6 +9,53 @@ class AADecay12new:
         self.A = np.zeros((4, 4))
         self.A[0, 0] = 1
     
+    def gen_2_other_based_on_1(var_name='aX', var_value=0.):
+        """_This function generates 2 parameters based on the input parameter value in a way that
+            the following condition is fulfilled:
+
+            aX^2 + aY^2 + aZ^2 = 1_
+
+            Through the var_name parameter we specify the parameter on the basis of which the others will be generated.
+
+        Args:
+            var_name (str, optional): _name of the chosen variable_. Defaults to 'aX'.
+            var_value (_float_, optional): _value of the chosen variable_. Defaults to 0..
+        
+        Returns:
+            _numpy.ndarray_: one-dimensional vector of parameters [aX, aY, aZ]__
+        """
+
+        match var_name:
+            case 'aX':
+                input_var = var_value
+                val = 1 - input_var** 2
+                other_var_gen_1 = np.random.uniform(0, val)
+                val2 = val - other_var_gen_1 ** 2
+                other_var_gen_2 = np.sqrt(val - other_var_gen_1 ** 2)
+                
+                return np.array([input_var, other_var_gen_1, other_var_gen_2])
+
+            case 'aY':
+                input_var = var_value
+                val = 1 - input_var** 2
+                other_var_gen_1 = np.random.uniform(0, val)
+                val2 = val - other_var_gen_1 ** 2
+                other_var_gen_2 = np.sqrt(val - other_var_gen_1 ** 2)
+                
+                return np.array([other_var_gen_2, input_var, other_var_gen_1])
+
+            case 'aZ':
+                input_var = var_value
+                val = 1 - input_var** 2
+                other_var_gen_1 = np.random.uniform(0, val)
+                val2 = val - other_var_gen_1 ** 2
+                other_var_gen_2 = np.sqrt(val - other_var_gen_1 ** 2)
+                
+                return np.array([other_var_gen_2, other_var_gen_1, input_var])
+            
+            case _:
+                print("Wrong variable name!")
+    
     def create_matrix(self):
         """_This functions creates a decay matrix. Martix elements
         are filled via class instance atributes._
